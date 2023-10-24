aeee68bcd72a1306464b4cec5d3d91523649dc34
mouhamadaloush <mouhamad.aloush06@gmail.com>
2023-10-24 12:59:08 +0300

    debug toolbar

--- CRM/urls.py
+++ CRM/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("website.urls"))
+    path("", include("website.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
