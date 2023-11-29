8395b506f6be013b7270054f471c0898ebb09b9a
mouhamadaloush <mouhamad.aloush06@gmail.com>
2023-11-29 22:30:04 +0300

    vercel conf

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 echo "building the project"
 python3 -m pip install -r requirements.txt
-
+echo "pip install done............"
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput --clear
