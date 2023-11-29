b49a4147792bcbfb0c556b704c72dd45501bc900
mouhamadaloush <mouhamad.aloush06@gmail.com>
2023-11-29 22:03:10 +0300

    vercel conf

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@
 
 python3 -m pip install -r requirements.txt
 
-pyhton3 manage.py makemigrations --noinput
-pyhton3 manage.py migrate --noinput
-pyhton3 manage.py collectstatic --noinput --clear
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput --clear
